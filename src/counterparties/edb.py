fb9e3a353d08c4f1ccc97c34a1a16067bacded84
napoknot21 <napoknot21@protonmail.com>
2025-10-06 17:30:11 +0200

    Extraction information for data (email) + has Attachment

--- src/counterparties/edb.py
+++ src/counterparties/edb.py
@@ -3,7 +3,56 @@ from __future__ import annotations
 import os
 import polars as pl
 
+from typing import Optional, Dict, Tuple, List
 from src.config import COUNTERPARTIES
 
 
 
+
+# ---------------------- Cash ----------------------
+
+
+
+def get_cash (dataframe : pl.DataFrame, md5 : Optional[str] = None, rules : Optional[Dict] = None) :
+    """
+    This function will return the cas for both fundations (HV, WR)
+    """
+    rules = COUNTERPARTIES if rules is None else rules
+    df_dicts = dataframe.to_dicts()
+
+    for row in df_dicts :
+        return None
+    
+    return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ---------------------- Collateral ----------------------
+
+
+def get_collateral (dataframe : pl.DataFrame, md5 : str) :
+    """
+    
+    """
+    return None
